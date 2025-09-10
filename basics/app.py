d578079a19a0779eecd5bd55d402a1c1bcf99ff3
biyash-1 <sthaalish39@gmail.com>
2025-09-10 20:52:27 +0545

    opencv

---
+++ basics/app.py
@@ -0,0 +1,144 @@
+# basics of python
+
+
+# print("hi python")
+
+
+# name = "biyash"
+# roll = 1
+# roll =2
+
+
+
+# first_name = "bs"
+# is_online = False
+# print(f"hi my name is {name} and my roll is :{roll}")
+# print(first_name)
+# print(is_online)
+
+
+# input
+
+# name = input('what is ur name')
+# print(name)
+# print("hello"+ name)
+
+
+# type
+# int
+# birth_year = input("enter the birth year")
+# age = 2025 - int(birth_year)
+# print(age)
+
+
+# float
+# birth_year = input("enter the birth year")
+# age = 2025 - float(birth_year)
+# print(age)
+
+
+# first = input('first')
+# second = input('second')
+# sum = int(first) + int(second)
+# print(sum)
+
+
+# since it is string we get 231
+
+course = 'python for begineer'
+
+
+print(course.find('for'))
+
+
+
+# arithmetic operations
+
+# print(11/2) - 5.5
+# print(11//2) - 5
+
+
+# x= 10
+# x = x+3
+# x += 3
+# print(x)
+
+# comparison
+# x = 3>2
+# print(x)
+
+price = 20
+print(price>30 and price>10)
+print(price>30 or price>10)
+
+
+
+# loops
+# temperature = 25
+# if temperature>40: 
+   
+
+#    print("its hot dat")
+
+# elif temperature >20:
+#      print("its nice weather")
+# else :
+#     print("cold")
+
+# print("done")   
+
+
+# exercise
+# weight = float(input("enter your weight:"))
+# type = input("(k)g or (L)bs:")
+
+# if type.upper() == "k":
+#    converted = weight/0.45
+#    print("weight in lbs" + str(converted))
+# else:
+#     converted = weight *0.45
+#     print(f"weight in kgs {converted}")
+
+
+# i =1
+# while i<=100:
+#     print(i)
+#     i = i+1
+
+# list
+# name = ["john","biaysh","ram"]
+
+# print(name[0:3])
+
+
+# numbers = [1,2,3,4,5]
+# numbers.extend([6,7])
+
+# print(numbers.count(9))
+# numbers.insert(0,4)
+# print(len(numbers))
+
+
+
+# for loops
+# number = [1,2,3,4,5]
+# for item in number:
+#     print(item)
+
+# i = 0
+# while  i <len(number):
+#     print(number[i])
+#     i = i+1  
+
+
+# range
+numbers = range(5,10)
+for number in numbers:
+    print(number)
+
+
+
+
+
+
+
